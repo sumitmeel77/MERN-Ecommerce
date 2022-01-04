780afa55bb5e42bd394b4265ea2120e832df1c8d
Sumit Meel <sumitmeel77@gmail.com>
2022-01-04 20:42:56 +0530

    creating api for product

--- Backend/controllers/productControllers.js
+++ Backend/controllers/productControllers.js
@@ -1,3 +1,58 @@
-exports.getAllProducts = (req, res) => {
-    res.status(200).json({ message: "working fine" })
+const Product = require("../models/productModel")
+
+//create product
+exports.createProduct = async (req, res, next) => {
+    const product = await Product.create(req.body)
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
+
+//get All products
+exports.getAllProducts = async (req, res) => {
+    const products = await Product.find()
+    res.status(200).json({
+        success: true,
+        products
+    })
+}
+
+//update product
+exports.UpdateProduct = async (req, res, next) => {
+    let product = Product.findById(req.params.id)
+    if (!product) {
+        return res.status(200).json({
+            success: true,
+            message: "product not found"
+        })
+    }
+    newProduct = await Product.findByIdAndUpdate(req.params.id, req.body,
+        {
+            new: true,
+            runValidators: true,
+            useFindandModifiy: false
+        })
+
+    res.status(200).json({
+        success: true,
+        newProduct
+    })
+}
+
+//deleting product
+exports.DeleteProduct = async (req, res, next) => {
+    let product = await Product.findByIdAndUpdate(req.params.id)
+    if (!product) {
+        return res.status(200).json({
+            success: true,
+            message: "product not found"
+        })
+    }
+    await product.remove();
+
+    res.status(200).json({
+        success: true,
+        message: "product deleted successfully"
+    })
 }
--- Backend/routers/productRoute.js
+++ Backend/routers/productRoute.js
@@ -1,9 +1,17 @@
 const express = require("express")
-const { getAllProducts } = require("../controllers/productControllers")
+const { getAllProducts, createProduct, UpdateProduct, DeleteProduct } = require("../controllers/productControllers")
 
 const router = express.Router()
 
 
-router.route("/products").get(getAllProducts)
+router.route("/products").get(getAllProducts)//api for getting all product
+
+
+router.route("/product/new").post(createProduct) // api for creating new product
+
+
+router.route("/product/:id").put(UpdateProduct)// api for updating product
+
+router.route("/product/:id").delete(DeleteProduct)// api for deleting product
 
 module.exports = router
